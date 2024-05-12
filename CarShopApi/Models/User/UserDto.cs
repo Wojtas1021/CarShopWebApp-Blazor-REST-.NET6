bdb21980be56515f4ce8d7df23eab5632c504e75
Wojtas1021 <porazinski.w@gmail.com>
2024-05-12 21:22:40 +0200

    Add user folder with Dto and AuthResponse classes, added static class CustomClaims to token authorization

---
+++ CarShopApi/Models/User/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarShopApi.Models.User
+{
+    public class UserDto : LoginUserDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
