e3d53052f246f5838c5827da644da36310053fdf
Wojtas1021 <porazinski.w@gmail.com>
2024-05-12 21:21:33 +0200

    intalled Identity nuget package, Jwt token, added Identity to DbContext class and program.cs, also appsettings.json

--- CarShopApi/Data/CarShopDbContext.cs
+++ CarShopApi/Data/CarShopDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarShopApi.Data
 {
-    public partial class CarShopDbContext : DbContext
+    public partial class CarShopDbContext : IdentityDbContext<ApiUser>
     {
         public CarShopDbContext()
         {
@@ -17,6 +19,8 @@ namespace CarShopApi.Data
         public virtual DbSet<Producer> Producers { get; set; } = null!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Car>(entity =>
             {
                 entity.HasIndex(e => e.Number, "UQ__Cars__78A1A19D234B0319")
@@ -52,6 +56,59 @@ namespace CarShopApi.Data
 
                 entity.Property(e => e.Name).HasMaxLength(50);
             });
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER",
+                    Id = "a890e379-b7c7-4b9b-a928-0e847f83f6a9"
+                },
+                new IdentityRole
+                {
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR",
+                    Id = "23caf552-39b5-4464-a221-37d264ce204c"
+                }
+            );
+            var hasher = new PasswordHasher<ApiUser>();
+            modelBuilder.Entity<ApiUser>().HasData(
+                new ApiUser
+                {
+                    Id = "55b4f1ed-8f01-4fea-a62a-1aa8cd9b6187",
+                    Email = "admin@carshop.com",
+                    NormalizedEmail = "ADMIN@CARSHOP.COM",
+                    UserName = "admin@carshop.com",
+                    NormalizedUserName = "ADMIN@CARSHOP.COM",
+                    Name = "System",
+                    Surname = "Admin",
+                    PasswordHash = hasher.HashPassword(null, "admin123")
+                },
+                new ApiUser
+                {
+                    Id = "d828ec99-761d-40a2-bae6-53e38cd37d5c",
+                    Email = "user@carshop.com",
+                    NormalizedEmail = "USER@CARSHOP.COM",
+                    UserName = "user@carshop.com",
+                    NormalizedUserName = "USER@CARSHOP.COM",
+                    Name = "System",
+                    Surname = "User",
+                    PasswordHash = hasher.HashPassword(null, "user123")
+                }
+            ); ;
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "a890e379-b7c7-4b9b-a928-0e847f83f6a9",
+                    UserId = "d828ec99-761d-40a2-bae6-53e38cd37d5c"
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = "23caf552-39b5-4464-a221-37d264ce204c",
+                    UserId = "55b4f1ed-8f01-4fea-a62a-1aa8cd9b6187"
+                }
+            );
 
             OnModelCreatingPartial(modelBuilder);
         }
--- CarShopApi/Program.cs
+++ CarShopApi/Program.cs
@@ -1,13 +1,21 @@
 using CarShopApi.Configurations;
 using CarShopApi.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connString = builder.Configuration.GetConnectionString("CarShopAppDbConnection");
 builder.Services.AddDbContext<CarShopDbContext>(options => options.UseSqlServer(connString));
+
+builder.Services.AddIdentityCore<ApiUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<CarShopDbContext>();
 
 //adding mapper class to services to gain use where ever needed 
 builder.Services.AddAutoMapper(typeof(MapperConfig));
@@ -28,7 +36,24 @@ builder.Services.AddCors(options => {
         .AllowAnyHeader()
         .AllowAnyOrigin());
 });
-
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]))
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,6 +66,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
