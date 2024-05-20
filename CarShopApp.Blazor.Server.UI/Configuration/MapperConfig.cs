03d7702f3a456e90f6fd4011c4fbfdac491970d7
Wojtas1021 <porazinski.w@gmail.com>
2024-05-20 20:39:03 +0200

    Added nuget for mapper and added mapperConfig class

---
+++ CarShopApp.Blazor.Server.UI/Configuration/MapperConfig.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CarShopApp.Blazor.Server.UI.Services.Base;
+
+namespace CarShopApp.Blazor.Server.UI.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<ProducerReadOnlyDto, ProducerUpdateDto>().ReverseMap();
+        }
+    }
+}
